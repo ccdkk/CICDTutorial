62745f0c35a4a976e689e19d780333748d733578
ccdkk <cdk243@gmail.com>
2022-11-18 10:43:06 +0900

    [set]: project 생성

---
+++ CICDTutorial/CICDTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDTutorial
+//
+//  Created by 최동권 on 2022/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
